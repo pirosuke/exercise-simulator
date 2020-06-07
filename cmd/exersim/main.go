ce56ed5e845803b55449b483320726deaa5678ca
htanaka <pirosukke@gmail.com>
2020-06-07 23:31:03 +0900

    updated to calc multiple plans.

--- cmd/exersim/main.go
+++ cmd/exersim/main.go
@@ -9,9 +9,9 @@ import (
 )
 
 /*
-WeeklyExercisePlans describes weekley exercising plans.
+ExercisePlans describes exercising plans.
 */
-type WeeklyExercisePlans struct {
+type ExercisePlans struct {
 	Title        string  `json:"title"`
 	METs         float64 `json:"mets"`
 	Hours        float64 `json:"hours"`
@@ -19,38 +19,94 @@ type WeeklyExercisePlans struct {
 }
 
 /*
+WeeklyPlans describes weekly exercising plans.
+*/
+type WeeklyPlans struct {
+	Name  string          `json:"name"`
+	Plans []ExercisePlans `json:"plans"`
+}
+
+/*
 PlansConfig describes setting data format.
 */
 type PlansConfig struct {
-	Weight        float64               `json:"weight"`
-	WeeksToOutput int64                 `json:"weeks_to_output"`
-	WeeklyPlans   []WeeklyExercisePlans `json:"weekly_plans"`
+	Weight        float64       `json:"weight"`
+	WeeksToOutput int64         `json:"weeks_to_output"`
+	WeeklyPlans   []WeeklyPlans `json:"weekly_plans"`
 }
 
 /*
-WeeklyResult is result data of week.
+ExerciseResult is calc result data of weekly exercise.
 */
-type WeeklyResult struct {
+type ExerciseResult struct {
 	Week         int64   `json:"week"`
 	StartWeight  float64 `json:"start_weight_kg"`
 	ExerciseKCal float64 `json:"exercise_kcal"`
-	RemovedFat   float64 `json:"removed_fat"`
+	RemovedFat   float64 `json:"removed_fat_kg"`
 }
 
 /*
-SimulatedResult is result data to output.
+WeeklyResult is result data of week
 */
-type SimulatedResult struct {
+type WeeklyResult struct {
+	Name       string           `json:"name"`
+	EndWeight  float64          `json:"end_weight_kg"`
+	RemovedFat float64          `json:"removed_fat_kg"`
+	Results    []ExerciseResult `json:"results"`
+}
+
+/*
+PlansSimulatedResult is result data to output.
+*/
+type PlansSimulatedResult struct {
 	Title         string         `json:"title"`
 	Weeks         int64          `json:"weeks"`
 	StartWeight   float64        `json:"start_weight_kg"`
-	EndWeight     float64        `json:"end_weight_kg"`
 	WeeklyResults []WeeklyResult `json:"weekly_results"`
 }
 
 func fileExists(filePath string) bool {
 	_, err := os.Stat(filePath)
 	return err == nil
+}
+
+func calcExerciseResult(startWeight float64, weeksToOutput int64, weeklyPlans WeeklyPlans) WeeklyResult {
+	remainingWeight := startWeight
+
+	var week int64
+	var exerciseResults []ExerciseResult
+	for week = 1; week <= weeksToOutput; week++ {
+		weeklyResult := ExerciseResult{
+			Week:        week,
+			StartWeight: remainingWeight,
+		}
+
+		weeklyExerciseKCal := 0.0
+		weeklyRemovedFat := 0.0
+		for _, plans := range weeklyPlans.Plans {
+			usedKCal := 1.05 * plans.METs * plans.Hours * remainingWeight * plans.TimesPerWeek
+			removedFat := usedKCal / 9 * 1.25 / 1000
+
+			weeklyExerciseKCal = weeklyExerciseKCal + usedKCal
+			weeklyRemovedFat = weeklyRemovedFat + removedFat
+
+		}
+
+		weeklyResult.ExerciseKCal = weeklyExerciseKCal
+		weeklyResult.RemovedFat = weeklyRemovedFat
+
+		exerciseResults = append(exerciseResults, weeklyResult)
+		remainingWeight = remainingWeight - weeklyRemovedFat
+	}
+
+	weeklyResult := WeeklyResult{
+		Name:       weeklyPlans.Name,
+		EndWeight:  remainingWeight,
+		RemovedFat: startWeight - remainingWeight,
+		Results:    exerciseResults,
+	}
+
+	return weeklyResult
 }
 
 func main() {
@@ -74,39 +130,17 @@ func main() {
 		return
 	}
 
-	result := SimulatedResult{}
+	result := PlansSimulatedResult{}
 	result.Title = *planFilePath
 	result.StartWeight = plansConfig.Weight
 	result.Weeks = plansConfig.WeeksToOutput
 
-	remainingWeight := plansConfig.Weight
-	var week int64
 	var weeklyResults []WeeklyResult
-	for week = 1; week <= plansConfig.WeeksToOutput; week++ {
-		weeklyResult := WeeklyResult{
-			Week:        week,
-			StartWeight: remainingWeight,
-		}
-
-		weeklyExerciseKCal := 0.0
-		weeklyRemovedFat := 0.0
-		for _, plans := range plansConfig.WeeklyPlans {
-			usedKCal := 1.05 * plans.METs * plans.Hours * remainingWeight * plans.TimesPerWeek
-			removedFat := usedKCal / 9 * 1.25 / 1000
-
-			weeklyExerciseKCal = weeklyExerciseKCal + usedKCal
-			weeklyRemovedFat = weeklyRemovedFat + removedFat
-
-		}
-
-		weeklyResult.ExerciseKCal = weeklyExerciseKCal
-		weeklyResult.RemovedFat = weeklyRemovedFat
-
+	for _, weeklyPlans := range plansConfig.WeeklyPlans {
+		weeklyResult := calcExerciseResult(plansConfig.Weight, plansConfig.WeeksToOutput, weeklyPlans)
 		weeklyResults = append(weeklyResults, weeklyResult)
-		remainingWeight = remainingWeight - weeklyRemovedFat
 	}
 
-	result.EndWeight = remainingWeight
 	result.WeeklyResults = weeklyResults
 
 	resultJSON, err := json.MarshalIndent(result, "", "    ")
